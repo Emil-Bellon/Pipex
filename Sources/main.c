ec801e944d039abc9e2620ec05b82c20dbef1c56
Emil Bellon <ebellon@z4r9p5.42lyon.fr>
2021-07-22 17:07:08 +0200

    errors gestion

--- Sources/main.c
+++ Sources/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lrobino <lrobino@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ebellon <ebellon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 19:23:24 by ebellon           #+#    #+#             */
-/*   Updated: 2021/06/20 21:42:50 by lrobino          ###   ########.fr       */
+/*   Updated: 2021/07/22 17:05:36 by ebellon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,6 +93,8 @@ int	exec_cmd(char *cmd, char **locations, char **envp)
 
 	exec_name = ft_strndup(cmd, ft_strnchr(cmd, ' '));
 	exec_path = get_exec_path(exec_name, locations);
+	if (exec_path == NULL)
+		dprintf(2, "pipex: command not found: %s\n", exec_name);
 	if (!exec_path)
 		return (1);
 	pid = fork();
@@ -102,6 +104,8 @@ int	exec_cmd(char *cmd, char **locations, char **envp)
 		exit(execve(exec_path, ft_split(cmd, ' '), envp));
 	else 
 		waitpid(pid, NULL, WNOHANG);
+	free(exec_name);
+	free(exec_path);
 	return (0);
 }
 
@@ -118,11 +122,8 @@ int exec_cmd_list(t_cmd_list *list, int size, char **location, char **envp)
 			return 1;
 		if (pid == 0)
 		{
-			//if (size != list->size)
-			{
-				close(pipe_fd[0]);
-				dup2(pipe_fd[1], STDOUT_FILENO);
-			}
+			close(pipe_fd[0]);
+			dup2(pipe_fd[1], STDOUT_FILENO);
 			exec_cmd_list(list, size - 1, location, envp);
 			close(pipe_fd[1]);
 			exit (0);
@@ -130,21 +131,27 @@ int exec_cmd_list(t_cmd_list *list, int size, char **location, char **envp)
 		else
 			waitpid(pid, 0, WNOHANG);
 	}
-	if (size != 1)
-	{
-		close(pipe_fd[1]);
-		dup2(pipe_fd[0], STDIN_FILENO);
-	}
+	close(pipe_fd[1]);
+	dup2(pipe_fd[0], STDIN_FILENO);
 	exec_cmd(list->list[size - 1], location, envp);
 	close(pipe_fd[0]);
 	return (0);
+}
+
+void	ft_free_tab(char **tab)
+{
+	int	i;
+	
+	i = 0;
+	while(tab[i])
+		free(tab[i++]);
+	free(tab);
 }
 
 int	main(int ac, char **av, char **envp)
 {
 	char	**locations;
 	int		i;
-	int		fd_pipe[2];
 	int		io_fd[2];
 	t_cmd_list	cmd_list = {
 		.list = av + 2,
@@ -155,12 +162,21 @@ int	main(int ac, char **av, char **envp)
 	locations = get_locations(envp);
 	if (!locations)
 		return (1);
+	pipe(io_fd);
 	io_fd[0] = open(av[1], O_RDONLY, 0644);
+	if (io_fd[0] < 0)
+		dprintf(2, "pipex: no such file or directory: %s\n", av[1]);
 	dup2(io_fd[0], STDIN_FILENO);
-	io_fd[1] = open(av[ac - 1], O_WRONLY | O_CREAT, 0644);
+	io_fd[1] = open(av[ac - 1], O_WRONLY | O_TRUNC | O_CREAT, 0644);
 	dup2(io_fd[1], STDOUT_FILENO);
+	if (io_fd[0] < 0)
+	{
+		cmd_list.list = av + 3;
+		cmd_list.size = ac - 4;
+	}
 	exec_cmd_list(&cmd_list, cmd_list.size, locations, envp);
-
-	close(fd_pipe[1]);
+	close(io_fd[0]);
+	close(io_fd[1]);
+	ft_free_tab(locations);
 	return (0);
 }
